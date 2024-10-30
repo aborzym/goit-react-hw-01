67f1e4e14a88e5981bfb17b5443ebf1f81c54278
Andrzej Borzym <borzym.a@gmail.com>
2024-10-30 15:01:57 +0100

    poprawka

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import userData from "../userData.json";
-import friends from "../friends.json";
 import transactions from "../transactions.json";
 
 import Profile from "./Profile";
